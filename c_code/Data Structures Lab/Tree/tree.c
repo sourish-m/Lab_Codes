9b5f73327c4b91fcfc7078737059d94f826a3205
Sourish-M <sourishmandaldgp@gmail.com>
2022-07-22 15:39:13 +0530

    Changed some files.

--- c_code/Data Structures Lab/Tree/tree.c
+++ c_code/Data Structures Lab/Tree/tree.c
@@ -3,90 +3,97 @@
 
 struct tree
 {
-    int num;
-    struct tree *left;
-    struct tree *right;
+	int num;
+	struct tree *left; //a pointer to the left node
+	struct tree *right;
 };
 
 typedef struct tree node;
 
+node* insert (node*, int);
+void inorder (node*);
+void preorder (node*);
+void postorder (node*);
+
 int main()
 {
-    node *insert (node*, int);
-    void inorder (node*);
-    void preorder (node*);
-    void postorder (node*);
-    node *root = NULL;
-    int x;
-    char ch = 'y';
+	node *root = NULL;
+	int x;
+	char ch = 'y';
 
-    while (ch == 'y')
-    {
-        printf("Enter the value to insert::");
-        scanf_s("%d", &x);
-        root = insert (root, x);
-        printf("\nNodes in inorder::");
-        inorder(root);
-        printf("\nNodes in preorder::");
-        preorder(root);
-        printf("\nNodes in postorder::");
-        postorder(root);
+	do
+	{
+		printf("Enter the value to insert::");
+		scanf_s("%d", &x);
+		root = insert (root, x);
+		printf("\nNodes in inorder::");
+		inorder(root);
+		printf("\nNodes in preorder::");
+		preorder(root);
+		printf("\nNodes in postorder::");
+		postorder(root);
         fflush(stdin);
-        printf("\nContinue?(y/n):");
-        scanf_s("%c", &ch);
-    }
+		printf("\nContinue?(y/n):");
+		scanf_s(" %c", &ch); // A space is put before %c so that any newline is skipped by scanf
+	}
+	while (ch == 'y');
 }
-node *insert ( node *root, int x)
+node *insert (node *root, int x)
 {
-    if (root == NULL)
-    {
-        root = (node *) malloc(sizeof(node));
-        root->num = x;
-        root->left = root->right = NULL;
-    }
-    else if (root->num > x)
-    { root->left = insert (root->left, x); }
-    else if (root->num < x)
-    { root->right = insert (root->right, x); }
-    else
-    { printf ("\nDuplicate"); }
+	if (root == NULL)
+	{
+		root = (node*) malloc(sizeof(node));
+		root->num = x;
+		root->left = root->right = NULL;
+	}
+	else if (root->num > x)
+	{
+		root->left = insert (root->left, x);
+	}
+	else if (root->num < x)
+	{
+		root->right = insert (root->right, x);
+	}
+	else
+	{
+		printf ("\nDuplicate");
+	}
 
-    return root;
-}
-
-
-void inorder (node *root)
-{
-    if (root != NULL)
-    {
-        inorder (root->left);
-        printf("%d ", root->num);
-        inorder(root->right);
-    }
-
-    return;
+	return root;
 }
 
-void preorder (node *root)
+void inorder (node* root)
 {
-    if (root != NULL)
-    {
-        printf("%d ", root->num);
-        preorder (root->left);
-        preorder(root->right);
-    }
+	if (root != NULL)
+	{
+		inorder (root->left);
+		printf("%d ", root->num);
+		inorder(root->right);
+	}
 
-    return;
+	return;
 }
 
-void postorder (node *root)
+void preorder (node* root)
 {
-    if (root != NULL)
-    {
-        postorder (root->left);
-        postorder (root->right);
-        printf("%d ", root->num);
-    }
+	if (root != NULL)
+	{
+		printf("%d ", root->num);
+		preorder (root->left);
+		preorder(root->right);
+	}
 
-    return;
+	return;
+}
+
+void postorder (node* root)
+{
+	if (root != NULL)
+	{
+		postorder (root->left);
+		postorder (root->right);
+		printf("%d ", root->num);
+	}
+
+	return;
 }
