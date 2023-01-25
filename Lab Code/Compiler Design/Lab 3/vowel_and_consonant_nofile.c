765aba30b1d8bc90daef9ebef66a420979a00a7a
Sourish-M <86039108+sourish-m@users.noreply.github.com>
2023-01-25 10:31:03 +0530

    changed indentation

--- Lab Code/Compiler Design/Lab 3/vowel_and_consonant_nofile.c
+++ Lab Code/Compiler Design/Lab 3/vowel_and_consonant_nofile.c
@@ -4,24 +4,30 @@
 #include <ctype.h>
 #include <string.h>
 
-int check_vowel (char c) {
+int check_vowel (char c)
+{
 	c = tolower(c);
 	if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
-		return 1; 
-    else return 0; }
-int main() {
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
 	char s[100], *word;
 	int line = 1;
 	printf("Enter sentences:\n");
-	while(scanf_s("%[^\n]%*c", s) == 1) {
+	while(fgets(s, 100, stdin)) {
 		word = strtok (s, " "); // break word into tokens
 		printf ("\nWords starting with vowel and ending with consonant are...\n");
 		while (word != NULL) {
 			if (check_vowel(word[0]) == 1 && check_vowel(word[strlen(word) - 1]) == 0) {
-					printf("%s", word);
-				printf("----> at line: %d\n", line); }
+				printf("%s", word);
+				printf("----> at line: %d\n", line);
+			}
 			word = strtok (NULL, " ");
 		}
 		printf("\n");
 		line++;
-	} }
+	}
+}
